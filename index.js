e2d50167d58feeff334644716f30f7b955234da1
Saket Singh <saketaryanite@gmail.com>
2023-10-16 15:10:29 +0530

    updates index.js 1

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const calculateExtraRunsIn2016 = require('./src/server/3-extra-runs-in-2016');
 const findTop10EconomicalBowlersIn2015 = require('./src/server/4-top-10-economical-bowlers-2015');
 const getTossWinnerMatchWinner = require('./src/server/5-toss-winner-match-winner');
 const getPlayerOfTheMatchBySeason = require('./src/server/6-player-Of-The-Match-By-Season');
-const calculateStrikeRateByBatsman = require('./src/server/7-strike-rate-by-batsman');
 const calculateStrikeRateByDhoni = require('./src/server/7-strike-rate-by-Dhoni');
 const getHighestDismissals = require('./src/server/8-highest-dismissals');
 const findBestEconomyInSuperOvers = require('./src/server/9-best-economy-in-super-overs');
@@ -35,7 +34,6 @@ fs.createReadStream(filePath_match)
         const result4 = findTop10EconomicalBowlersIn2015(matchesData, deliveriesData);
         const result5 = getTossWinnerMatchWinner(matchesData);
         const result6 = getPlayerOfTheMatchBySeason(matchesData);
-        const result7_all = calculateStrikeRateByBatsman(deliveriesData, matchesData, uniquePlayers);
         const result7_Dhoni = calculateStrikeRateByDhoni(deliveriesData, matchesData);
         const result8 = getHighestDismissals(deliveriesData);
         const result7 = findBestEconomyInSuperOvers(deliveriesData);
@@ -46,7 +44,6 @@ fs.createReadStream(filePath_match)
         fs.writeFileSync('/home/saket10/Documents/mountblue/Project_IPL/src/public/output/top10EconomicalBowlers2015.json', JSON.stringify(result4, null, 2));
         fs.writeFileSync('/home/saket10/Documents/mountblue/Project_IPL/src/public/output/tossWinnerMatchWinner.json', JSON.stringify(result5, null, 2));
         fs.writeFileSync('/home/saket10/Documents/mountblue/Project_IPL/src/public/output/playerOfTheMatchBySeason.json', JSON.stringify(result6, null, 2));
-        fs.writeFileSync('/home/saket10/Documents/mountblue/Project_IPL/src/public/output/strikeRateByBatsman.json', JSON.stringify(result7_all, null, 2));
         fs.writeFileSync('/home/saket10/Documents/mountblue/Project_IPL/src/public/output/strikeRateByDhoni.json', JSON.stringify(result7_Dhoni, null, 2));
         fs.writeFileSync('/home/saket10/Documents/mountblue/Project_IPL/src/public/output/highestDismissals.json', JSON.stringify(result8, null, 2));
         fs.writeFileSync('/home/saket10/Documents/mountblue/Project_IPL/src/public/output/bestEconomyInSuperOvers.json', JSON.stringify(result7, null, 2));
@@ -58,7 +55,6 @@ fs.createReadStream(filePath_match)
         console.log('Top 10 economical bowlers in the year 2015 data has been generated and saved to public/output/top10EconomicalBowlers2015.json');
         console.log('Number of times each team won the toss and also won the match data has been generated and saved to public/output/tossWinnerMatchWinner.json');
         console.log('Player with the highest number of Player of the Match awards for each season data has been generated and saved to public/output/playerOfTheMatchBySeason.json');
-        console.log('Strike rate of batsmen for each season data has been generated and saved to public/output/strikeRateByBatsman.json');
         console.log('Strike rate of Dhoni for each season data has been generated and saved to public/output/strikeRateByDhoni.json');
         console.log('Highest number of times one player has been dismissed by another player data has been generated and saved to public/output/highestDismissals.json');
         console.log('Bowler with the best economy in super overs data has been generated and saved to public/output/bestEconomyInSuperOvers.json');
