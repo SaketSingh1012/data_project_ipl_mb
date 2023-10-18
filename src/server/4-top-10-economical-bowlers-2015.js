99fde8a3fecd7e5c67bcbee09992b1374d9cffe7
Saket Singh <saketaryanite@gmail.com>
2023-10-18 17:32:00 +0530

    updated

--- src/server/4-top-10-economical-bowlers-2015.js
+++ src/server/4-top-10-economical-bowlers-2015.js
@@ -6,8 +6,12 @@ function findTop10EconomicalBowlersIn2015(matchesData, deliveriesData) {
     const matchId = row.match_id;
     const bowler = row.bowler;
     const wideRuns = parseInt(row.wide_runs, 10);
+    const legByeRuns = parseInt(row.legbye_runs, 10);
+    const byeRuns = parseInt(row.bye_runs, 10);
     const noballRuns = parseInt(row.noball_runs, 10);
-    const total_conceeded_runs=parseInt(row.total_runs,10);
+    const penaltyRuns = parseInt(row.penalty_runs, 10);
+    const totalConcededRuns = parseInt(row.total_runs, 10);
+
     const match = matches2015.find((m) => m.id === matchId);
 
     if (match) {
@@ -18,9 +22,8 @@ function findTop10EconomicalBowlersIn2015(matchesData, deliveriesData) {
         };
       }
 
-
-      bowlerData[bowler].totalRuns +=total_conceeded_runs;
-      if(!(wideRuns || noballRuns)){
+      bowlerData[bowler].totalRuns += totalConcededRuns - legByeRuns - byeRuns - penaltyRuns;
+      if (!(wideRuns || noballRuns)) {
         bowlerData[bowler].totalBalls++;
       }
     }
@@ -33,7 +36,7 @@ function findTop10EconomicalBowlersIn2015(matchesData, deliveriesData) {
     const runs = bowlerData[bowler].totalRuns;
     const balls = bowlerData[bowler].totalBalls;
 
-    if (balls >0) {
+    if (balls > 0) {
       const economy = ((runs / balls) * 6).toFixed(2);
       economicalBowlers.push({
         bowler,
